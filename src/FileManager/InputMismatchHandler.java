ceb11551acc8f15d571a26ccf834e8e6d071650c
Milkweedz <Milkweedz@hotmail.com>
2018-11-16 13:59:28 +0800

    Created javadoc comments in InputMismatchHandler class

--- src/FileManager/InputMismatchHandler.java
+++ src/FileManager/InputMismatchHandler.java
@@ -3,7 +3,21 @@ package FileManager;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * This class handles all of the input mismatches. When the application prompts the user to enter data of a certain type
+ * and the user enters a different type, methods in this class will return an error message and force the user to enter
+ * the correct data type.
+ *
+ * @author Ng Man Chun
+ * @version 1.0
+ * @since 2018-11-15
+ */
 public class InputMismatchHandler {
+
+    /**
+     * This method ensures that the value input is an integer.
+     * @return integer value
+     */
     public int checkInt(){
         Scanner scan = new Scanner(System.in);
         int value = 0;
@@ -22,6 +36,13 @@ public class InputMismatchHandler {
         return value;
     }
 
+    /**
+     * This method ensures that the value input is an integer and in a specific range.
+     * It is an overloaded version of checkInt(void)
+     * @param min
+     * @param max
+     * @return integer value
+     */
     public int checkInt(int min, int max){
         int value;
 
@@ -34,6 +55,10 @@ public class InputMismatchHandler {
         return value;
     }
 
+    /**
+     * This method ensures that the value input is an float.
+     * @return float value
+     */
     public float checkFloat(){
         Scanner scan = new Scanner(System.in);
         float value = 0;
@@ -52,6 +77,13 @@ public class InputMismatchHandler {
         return value;
     }
 
+    /**
+     * This method ensures that the value input is a float and in a specific range.
+     * It is an overloaded version of checkFloat(void)
+     * @param min
+     * @param max
+     * @return float value
+     */
     public float checkFloat(float min, float max){
         float value;
 
@@ -64,6 +96,12 @@ public class InputMismatchHandler {
         return value;
     }
 
+    /**
+     * This method ensures that the string input matches one of the strings given in the options string array.
+     * It prevents users from inputting a string that is not valid.
+     * @param options
+     * @return a valid string
+     */
     public String checkString(String[] options){
         Scanner scan = new Scanner(System.in);
         String value = "";
